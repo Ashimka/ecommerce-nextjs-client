947ffca983360005f7a9f10901f3a2ce5c8e5cf0
Ashimka <ashimka2010@yandex.ru>
2024-04-07 18:01:43 +0500

    add: auth page

---
+++ src/components/AuthPage/AuthPage.tsx
@@ -0,0 +1,116 @@
+"use client";
+import Image from "next/image";
+import React, { MutableRefObject, useRef } from "react";
+
+import styles from "@/styles/auth/index.module.scss";
+
+const AuthPage = () => {
+  const container = useRef() as MutableRefObject<HTMLDivElement>;
+
+  const switchForm = () => {
+    container.current.classList.toggle(styles.right_panel_active);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container} ref={container} id="container">
+        <div className={`${styles.form_container} ${styles.sign_up_container}`}>
+          <form className={styles.form}>
+            <h1 className={styles.h1}>Создать аккаунт</h1>
+            <div className={styles.social_container}>
+              <a href="#" className={styles.social}>
+                <Image
+                  src="img/vk.svg"
+                  alt="Логотип в контактк"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a href="#" className={styles.social}>
+                <Image
+                  src="img/ya.svg"
+                  alt="Логотип в контактк"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </div>
+            <span className={styles.span}>
+              или используйте свой email для регистрации
+            </span>
+            <input className={styles.input} type="text" placeholder="Телефон" />
+            <input className={styles.input} type="email" placeholder="Email" />
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Пароль"
+            />
+            <button className={styles.button}>Зарегистрировать</button>
+          </form>
+        </div>
+        <div className={`${styles.form_container} ${styles.sign_in_container}`}>
+          <form className={styles.form}>
+            <h1 className={styles.h1}>Войти</h1>
+            <div className={styles.social_container}>
+              <a href="#" className={styles.social}>
+                <Image
+                  src="img/vk.svg"
+                  alt="Логотип в контактк"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a href="#" className={styles.social}>
+                <Image
+                  src="img/ya.svg"
+                  alt="Логотип в контактк"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </div>
+            <span className={styles.span}>
+              или используйте свою учетную запись
+            </span>
+            <input className={styles.input} type="text" placeholder="Телефон" />
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Пароль"
+            />
+            <a href="#">Забыли пароль?</a>
+            <button className={styles.button}>Войти</button>
+          </form>
+        </div>
+        <div className={styles.overlay_container}>
+          <div className={styles.overlay}>
+            <div className={`${styles.overlay_panel} ${styles.overlay_left}`}>
+              <h1 className={styles.h1}>Добро пожаловать!</h1>
+              <p className={styles.p}>Зарегистрированны? Войдите</p>
+              <button
+                className={`${styles.button} ${styles.ghost}`}
+                onClick={switchForm}
+                id="signIn"
+              >
+                Вход
+              </button>
+            </div>
+            <div className={`${styles.overlay_panel} ${styles.overlay_right}`}>
+              <h1 className={styles.h1}>Привет!</h1>
+              <p className={styles.p}>Зарегистрируйтесь на сайте</p>
+              <button
+                className={`${styles.button} ${styles.ghost}`}
+                onClick={switchForm}
+                id="signUp"
+              >
+                Регистрация
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthPage;
